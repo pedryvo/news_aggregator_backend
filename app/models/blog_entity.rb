e191792cb8b293c64008a5bdad68e5d909fa48e3
Pedro Ivo <pedryvo@gmail.com>
2021-02-02 02:35:39 -0300

    add rescue to fetch_news

--- app/models/blog_entity.rb
+++ app/models/blog_entity.rb
@@ -3,18 +3,22 @@ class BlogEntity < ApplicationRecord
   has_many :posts
 
   def fetch_news
-    rss = RestClient.get self.base_url
     
-    feed = SimpleRSS.parse rss
+    begin
+      rss = RestClient.get self.base_url
+      feed = SimpleRSS.parse rss
     
-    feed.items.each do |item|
-      fetch_post = LinkThumbnailer.generate(item.link.include?('?') ?  item.link[/[^?]+/] : item.link)
-      post_title = fetch_post.title
-      post_url = fetch_post.url.to_s
-      post_featured_image = fetch_post.images.first.src
-      post_description = fetch_post.description + '...'
+      feed.items.each do |item|
+        fetch_post = LinkThumbnailer.generate(item.link.include?('?') ?  item.link[/[^?]+/] : item.link)
+        post_title = fetch_post.title
+        post_url = fetch_post.url.to_s
+        post_featured_image = fetch_post.images.first.src
+        post_description = fetch_post.description + '...'
 
-      Post.create!(title: post_title, featured_image_url: post_featured_image, url: post_url, description: post_description, blog_entity: self) unless Post.exists?(title: post_title)
+        Post.create!(title: post_title, featured_image_url: post_featured_image, url: post_url, description: post_description, blog_entity: self) unless Post.exists?(title: post_title)
+      end
+    rescue => e
+      puts "Rescued: #{e.inspect}"
     end
   end
 end
