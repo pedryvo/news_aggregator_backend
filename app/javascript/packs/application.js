83a69f5f88ab80160a9237bec715b019f0e07793
Pedro Ivo <pedryvo@gmail.com>
2021-02-02 15:54:26 -0300

    update layout to material design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,10 +9,42 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 import 'bootstrap/dist/js/bootstrap.bundle'
-// import 'bootstrap/dist/css/bootstrap'
-require("stylesheets/bootstrap.min.css")
+import 'mdbootstrap/css/bootstrap'
+import 'mdbootstrap/css/mdb'
+import '@fortawesome/fontawesome-free/js/all'
+
 require("stylesheets/application.scss")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+$( document ).on('turbolinks:load', function() {
+
+  // Carousel
+
+  $(".carousel").carousel({
+      interval: false,
+      pause: true
+  });
+
+  $( ".carousel .carousel-inner" ).swipe( {
+  swipeLeft: function ( event, direction, distance, duration, fingerCount ) {
+      this.parent( ).carousel( 'next' );
+  },
+  swipeRight: function ( ) {
+      this.parent( ).carousel( 'prev' );
+  },
+  threshold: 0,
+  tap: function(event, target) {
+      window.location = $(this).find('.carousel-item.active a').attr('href');
+  },
+  excludedElements:"label, button, input, select, textarea, .noSwipe"
+  } );
+
+  $('.carousel .carousel-inner').on('dragstart', 'a', function () {
+      return false;
+  });  
+
+});
--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module VcaNews
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Brasilia"
+    config.i18n.default_locale = :"pt-BR"
   end
 end
