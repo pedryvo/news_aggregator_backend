9423132a4ed4234999deabf2124a05dd5a9590ec
Pedro Ivo <pedryvo@gmail.com>
2021-02-04 01:07:52 -0300

    create posts API endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   resources :cities do
     resources :posts
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+    namespace :v1 do
+      resources :cities do
+        resources :posts, only: [:index]
+      end
+    end
+  end
 end
