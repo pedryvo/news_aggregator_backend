3c948f124cdb5743f062fb521d5dfec0dbde58f5
Pedro Ivo <pedryvo@gmail.com>
2021-02-06 03:59:03 -0300

    add city to serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,3 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :featured_image_url, :url, :blog_entity, :created_at
+  attributes :id, :title, :description, :featured_image_url, :url, :blog_entity, :city, :created_at
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_212119) do
+ActiveRecord::Schema.define(version: 2021_02_06_040521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -120,6 +120,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_212119) do
     t.integer "state_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "app_name"
   end
 
   create_table "posts", force: :cascade do |t|
