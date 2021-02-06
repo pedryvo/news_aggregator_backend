83d883eeecb22e6daccce2b47586da1dda064556
Pedro Ivo <pedryvo@gmail.com>
2021-02-06 00:25:27 -0300

    add pagination to posts controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,9 +1,18 @@
+ApiPagination.configure do |config|
+  config.paginator = :pagy
+end
+
+include Pagy::Backend
+include Rails::Pagination
+
 module Api
   module V1
     class PostsController < ActionController::Base
       def index
         city = City.find(params[:city_id])
-        @posts = city.posts.last(5)
+        @posts = city.posts
+
+        paginate json: @posts, per_page: 30
       end
     end
   end
