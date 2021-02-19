842972577469e0b2bc984a7dbbe85beb39175c77
Pedro Ivo <pedryvo@gmail.com>
2021-02-19 16:03:54 -0300

    rename models

---
+++ app/models/old_post.rb
@@ -0,0 +1,11 @@
+class OldPost < ApplicationRecord
+  belongs_to :blog_entity
+
+  delegate :company, to: :employee, allow_nil: true
+
+  attribute :city
+
+  def city
+    self.blog_entity.city
+  end
+end
