67f09133f88eb07e526918b477198c5ac458356a
Pedro Ivo <pedryvo@gmail.com>
2021-02-21 05:18:42 -0300

    add delayed job to news crawler

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_040521) do
+ActiveRecord::Schema.define(version: 2021_02_21_081132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 2021_02_06_040521) do
     t.bigint "author_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
 
   create_table "active_admin_managed_resources", force: :cascade do |t|
@@ -123,6 +123,32 @@ ActiveRecord::Schema.define(version: 2021_02_06_040521) do
     t.string "app_name"
   end
 
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "featured_image_url"
+    t.string "url"
+    t.bigint "blog_entity_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
+    t.index ["blog_entity_id"], name: "index_posts_on_blog_entity_id"
+  end
+
   create_table "regions", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -139,4 +165,5 @@ ActiveRecord::Schema.define(version: 2021_02_06_040521) do
 
   add_foreign_key "admin_users", "cities"
   add_foreign_key "blog_entities", "cities"
+  add_foreign_key "posts", "blog_entities"
 end
