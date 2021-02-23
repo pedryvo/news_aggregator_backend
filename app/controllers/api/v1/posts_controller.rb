915e427aa8592495fb7a43cc1d3da59b5703a104
Pedro Ivo <pedryvo@gmail.com>
2021-02-23 14:37:00 -0300

    fix order mongoid posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,7 +5,7 @@ module Api
         expires_in 1.minute, public: true
         params.permit(:page)
         city = City.find(params[:city_id])
-        @posts_response = city.posts.page(params[:page].to_i).per(10)
+        @posts_response = city.posts.order_by(id: -1).page(params[:page].to_i).per(10)
         render json: @posts_response
       end
     end
